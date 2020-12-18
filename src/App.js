f754156c1f39407e4cc4b73aa7fc69f642297dee
Yasir-22 <yasirmoiz01@gmail.com>
2020-12-18 15:45:30 +0500

    assignemnt 4 using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+import './App.css';
+import Message from './message'
+
+function App() {
+  let [count,setcount]=useState(1)
+  let[morning,setmorning]=useState(true)
+  return (
+  <div className={`box ${morning ? 'daylight': '' }`}>
+    <h1>It is {morning ? ' morning time' : " night time"}</h1>
+    <Message counter={count} />
+   <button onClick={()=>{setcount(++count)}}>click here to count</button>
+    <button onClick={()=>{setmorning(!morning)}}>chagne time</button>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/message.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Message(props){
+    return(
+<h1>The counter value is {props.counter}</h1>    )
+
+}
+
+
