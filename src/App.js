cd71a86c9011a1ca5785b713ebce6b93d2f6e10b
Yasir-22 <yasirmoiz01@gmail.com>
2020-12-18 21:23:34 +0500

    again update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   let[morning,setmorning]=useState(true)
   return (
   <div className={`box ${morning ? 'daylight': '' }`}>
-    <h1>It is what morning or night? {morning ? ' morning time' : " night time"}</h1>
+    <h1>It is what morning or night ? =>  {morning ? ' morning time' : " night time"}</h1>
     <Message counter={count} />
    <button onClick={()=>{setcount(++count)}}>click here to count</button>
     <button onClick={()=>{setmorning(!morning)}}>chagne time</button>
